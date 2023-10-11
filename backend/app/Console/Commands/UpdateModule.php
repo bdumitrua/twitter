3e6cb734a2a2b9a8d1e3ebd95cffd241a20dc93c
bdumitrua <bdumitrua@gmail.com>
2023-10-11 21:48:25 +0300

    added update all modules command

---
+++ backend/app/Console/Commands/UpdateModule.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use DirectoryIterator;
+use Illuminate\Console\Command;
+
+class UpdateModule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:module';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update existing modules';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $moduleDir = app_path() . '/Modules/';
+
+        // Список всех папок, которые должны быть в модулях
+        $dirs = ['Models', 'Repositories', 'Jobs', 'Events'];
+
+        foreach (new DirectoryIterator($moduleDir) as $fileInfo) {
+            if ($fileInfo->isDot()) continue;
+            if ($fileInfo->isDir()) {
+                $moduleName = $fileInfo->getFilename();
+
+                foreach ($dirs as $dir) {
+                    $path = $moduleDir . "/$moduleName/$dir";
+
+                    if (!is_dir($path)) {
+                        mkdir($path, 0777, true);
+                        $fileName = "{$moduleName}{$dir}.php";
+                        $fileContent = $this->generateFileContent($moduleName, $dir);
+                        file_put_contents("{$path}/{$fileName}", $fileContent);
+                        $this->info("Added $dir to $moduleName");
+                    }
+                }
+            }
+        }
+
+        $this->info("Modules updated successfully.");
+    }
+
+    private function generateFileContent($moduleName, $dir)
+    {
+        $className = "{$moduleName}{$dir}";
+
+        return "<?php
+
+namespace App\\Modules\\{$moduleName}\\{$dir};
+
+class {$className}
+{
+    // TODO: Implement your class logic here
+}
+";
+    }
+}
