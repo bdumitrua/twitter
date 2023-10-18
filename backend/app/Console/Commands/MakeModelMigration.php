3278edfd968911f1a459cb0308cddea929e91b6a
bdumitrua <bdumitrua@gmail.com>
2023-10-18 22:14:30 +0300

    created app/Console/Commands/MakeModelMigration

---
+++ backend/app/Console/Commands/MakeModelMigration.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class MakeModelMigration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:model:migration {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates migration, factory and seeder for model';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // UserGroup
+        $model = $this->argument('model');
+        $model_snake_case = $this->pascalToSnake($model);
+        Artisan::call('make:migration', [NAME => "create_{$model_snake_case}s_table"]);
+        Artisan::call('make:factory', [NAME => "{$model}Factory"]);
+        Artisan::call('make:seeder', [NAME => "{$model}TableSeeder"]);
+    }
+
+    protected function pascalToSnake($string)
+    {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $string));
+    }
+}
