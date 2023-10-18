fecbcb3678c2ad73b470f921dd714b4d8eb28bfa
bdumitrua <bdumitrua@gmail.com>
2023-10-18 23:14:45 +0300

    changed invalid login error

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Invalid credentials'], 400);
         }
 
         return $this->respondWithToken($token);
