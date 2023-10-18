0581f37c52e64f10fd2bc881613c656c881a9677
bdumitrua <bdumitrua@gmail.com>
2023-10-18 11:43:59 +0300

    added base query for future

--- backend/app/Modules/User/Repositories/UserRepository.php
+++ backend/app/Modules/User/Repositories/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Modules\User\Repositories;
 use App\Modules\User\Models\User;
 use Elastic\Elasticsearch\Client as ElasticSearch;
 use Elastic\ScoutDriverPlus\Support\Query;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 
 class UserRepository
@@ -20,9 +21,19 @@ class UserRepository
         $this->elasticSearch = $elasticSearch;
     }
 
+    protected function baseQuery(): Builder
+    {
+        return $this->user->newQuery();
+    }
+
+    protected function queryById(int $id): Builder
+    {
+        return $this->baseQuery()->where('id', '=', $id);
+    }
+
     public function getById(int $id)
     {
-        return $this->user->where('id', '=', $id);
+        return $this->baseQuery()->whereId($id)->first();
     }
 
     public function search(string $text): Collection
