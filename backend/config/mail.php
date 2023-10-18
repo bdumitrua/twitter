1113fdaea1c6bb810f26bbda8b8612fb96c93276
bdumitrua <bdumitrua@gmail.com>
2023-10-18 22:12:48 +0300

    constant hotfix

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -100,7 +100,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        NAME => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
