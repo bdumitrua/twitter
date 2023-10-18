a07659990672c183b0796c4374b1a6e8ebb3b714
bdumitrua <bdumitrua@gmail.com>
2023-10-18 14:46:27 +0300

    added seeding and sm fix subscribtion logic

--- backend/database/factories/UserSubscribtionFactory.php
+++ backend/database/factories/UserSubscribtionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Modules\User\Models\User;
 use App\Modules\User\Models\UserSubscribtion;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -18,8 +19,12 @@ class UserSubscribtionFactory extends Factory
 
     public function definition(): array
     {
+        $subscriber = User::all()->random();
+        $user = User::where('id', '!=', $subscriber->id)->get()->random();
+
         return [
-            // 
+            'subscriber_id' => $subscriber->id,
+            'user_id' => $user->id,
         ];
     }
 }
