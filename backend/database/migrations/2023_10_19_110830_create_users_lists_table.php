6d9c9b6b45be4f130146a3cbe11486129f3125ed
bdumitrua <bdumitrua@gmail.com>
2023-10-19 15:18:25 +0300

    users lists hotfix + did users slists request

--- backend/database/migrations/2023_10_19_110830_create_users_lists_table.php
+++ backend/database/migrations/2023_10_19_110830_create_users_lists_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger(USER_ID);
+            $table->string(DESCRIPTION);
             $table->string('bg_image');
             $table->boolean('is_private');
             $table->integer('subsribers_count')->default(0);
