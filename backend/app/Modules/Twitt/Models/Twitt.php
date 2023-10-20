826eaeebbe2f119c537c85c81f3b71253ccec16b
bdumitrua <bdumitrua@gmail.com>
2023-10-20 13:01:05 +0300

    wrote twitt model and migration

--- backend/app/Modules/Twitt/Models/Twitt.php
+++ backend/app/Modules/Twitt/Models/Twitt.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Twitt\Models;
 
+use Database\Factories\TwittFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,10 +11,23 @@ class Twitt extends Model
     use HasFactory;
 
     protected $fillable = [
-        // Put your changeable fields here
+        USER_ID,
+        USER_GROUP_ID,
+        'text',
+        'likes_count',
+        'reposts_count',
+        'replies_count',
+        'favorites_count',
+        'is_comment',
+        'commented_twitt_id',
+        'is_quoute',
+        'quoted_twitt_id',
+        'is_repost',
+        'reposted_twitt_id',
     ];
 
-    protected $hidden = [
-        // The attributes that should be hidden for serialization
-    ];
+    protected static function newFactory()
+    {
+        return TwittFactory::new();
+    }
 }
--- backend/database/factories/TwittFactory.php
+++ backend/database/factories/TwittFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Modules\Twitt\Models\Twitt;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class TwittFactory extends Factory
 {
+    protected $model = Twitt::class;
+
     /**
      * Define the model's default state.
      *
