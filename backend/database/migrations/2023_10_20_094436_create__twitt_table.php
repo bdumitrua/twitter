ce808645f479e1eac1f5a3d98c8fc7ef834b0d60
bdumitrua <bdumitrua@gmail.com>
2023-10-20 13:01:05 +0300

    wrote twitt model and migration

--- backend/database/migrations/2023_10_20_094436_create__twitt_table.php
+++ backend/database/migrations/2023_10_20_094436_create__twitt_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('twitts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger(USER_ID);
+            $table->unsignedBigInteger(USER_GROUP_ID);
+            $table->string('text');
+            $table->integer('likes_count');
+            $table->integer('reposts_count');
+            $table->integer('replies_count');
+            $table->integer('favorites_count');
+            $table->boolean('is_comment');
+            $table->unsignedBigInteger('commented_twitt_id');
+            $table->boolean('is_quoute');
+            $table->unsignedBigInteger('quoted_twitt_id');
+            $table->boolean('is_repost');
+            $table->unsignedBigInteger('reposted_twitt_id');
             $table->timestamps();
+
+            $table->foreign(USER_ID)->references('id')->on('users')->onDelete('cascade');
+            $table->foreign(USER_GROUP_ID)->references('id')->on('user_groups')->onDelete('cascade');
+            $table->foreign('commented_twitt_id')->references('id')->on('twitts')->onDelete('cascade');
+            $table->foreign('quoted_twitt_id')->references('id')->on('twitts')->onDelete('cascade');
+            $table->foreign('reposted_twitt_id')->references('id')->on('twitts')->onDelete('cascade');
         });
     }
 
