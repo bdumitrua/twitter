38d22276dc1e5ae09e3d1afaa9955917c12ff1fc
bdumitrua <bdumitrua@gmail.com>
2023-10-23 01:24:42 +0300

    added forever option in getCachedData

--- backend/app/Traits/GetCachedData.php
+++ backend/app/Traits/GetCachedData.php
@@ -6,14 +6,19 @@ use Illuminate\Support\Facades\Cache;
 
 trait GetCachedData
 {
-    private function getCachedData(string $key, callable $callback, int $seconds = 60)
+    private function getCachedData(string $key, callable $callback, ?int $seconds = 60)
     {
         if ($cachedData = Cache::get($key)) {
             return $cachedData;
         }
 
         $data = $callback();
-        Cache::put($key, $data, now()->addSeconds($seconds));
+
+        if (!empty($seconds)) {
+            Cache::put($key, $data, now()->addSeconds($seconds));
+        } else {
+            Cache::forever($key, $data);
+        }
 
         return $data;
     }
