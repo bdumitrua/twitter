c85e53cd0985135652275753d21d9458eacdc31d
carasique22 <timofeidudin@mail.ru>
2023-11-16 18:38:13 +0300

    tweet page with no comments

--- frontend/src/pages/TweetPage/TweetPage.jsx
+++ frontend/src/pages/TweetPage/TweetPage.jsx
@@ -16,53 +16,55 @@ const TweetPage = () => {
         <div className={styles["tweet"]}>
           {/* <TweetAdditional /> */}
           <div className={styles["tweet__wrapper"]}>
-            <div className={styles["tweet__image"]}>
-              <img
-                className={styles["tweet__user-avatar"]}
-                src={userPhoto}
-                alt=""
-              />
-            </div>
             <div className={styles["tweet__content"]}>
               <div className={styles["tweet__user-info"]}>
-                <span className={styles["tweet__username"]}>Martha Craig</span>
-                <span className={styles["tweet__nickname"]}>@craig_love</span>
+                <div className={styles["tweet__image"]}>
+                  <img
+                    className={styles["tweet__user-avatar"]}
+                    src={userPhoto}
+                    alt=""
+                  />
+                </div>
+                <div className={styles["tweet__names"]}>
+                  {" "}
+                  <span className={styles["tweet__username"]}>
+                    Martha Craig
+                  </span>
+                  <span className={styles["tweet__nickname"]}>@craig_love</span>
+                </div>
               </div>
               <div className={styles["tweet__tweet-body"]}>
-                <span className={styles["tweet__text"]}></span>
+                <span className={styles["tweet__text"]}>
+                  ~~ hiring for a UX Lead in Sydney - who should I talk to?
+                </span>
+              </div>
+              <div className={styles["tweet__time-date"]}>
+                <div className={styles["tweet__time"]}>09:28</div>
+                <div className={styles["tweet__date"]}>· 2/21/20</div>
               </div>
               <div className={styles["tweet__counters"]}>
-                <a className={styles["tweet__counter"]} href="#/">
-                  <img
-                    className={styles["tweet__counter-logo"]}
-                    src={comment}
-                    alt=""
-                  />
-                  28
-                </a>
-                <a className={styles["tweet__counter"]} href="#/">
-                  <img
-                    className={styles["tweet__counter-logo"]}
-                    src={retweet}
-                    alt=""
-                  />
-                  5
-                </a>
-                <a className={styles["tweet__counter"]} href="#/">
-                  <img
-                    className={styles["tweet__counter-logo"]}
-                    src={unpaintedLike}
-                    alt=""
-                  />
-                  21
-                </a>
-                <a className={styles["tweet__counter"]} href="#/">
-                  <img
-                    className={styles["tweet__conter-logo"]}
-                    src={makeRepost}
-                    alt=""
-                  />
-                </a>
+                <div className={styles["tweet__retweets"]}>
+                  <div className={styles["tweet__counter"]}>6</div>
+                  Retweets
+                </div>
+                <div className={styles["tweet__likes"]}>
+                  <div className={styles["tweet__counter"]}>15</div>
+                  Likes
+                </div>
+              </div>
+              <div className={styles["tweet__buttons"]}>
+                <div className={styles["tweet__button"]}>
+                  <img src={comment} alt="" />
+                </div>
+                <div className={styles["tweet__button"]}>
+                  <img src={retweet} alt="" />
+                </div>
+                <div className={styles["tweet__button"]}>
+                  <img src={unpaintedLike} alt="" />
+                </div>
+                <div className={styles["tweet__button"]}>
+                  <img src={makeRepost} alt="" />
+                </div>
               </div>
             </div>
           </div>
