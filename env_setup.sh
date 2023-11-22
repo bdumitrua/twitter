b5c0d52ee66b818dde8dd1c5cf2fa0f3912c6e3e
bdumitrua <bdumitrua@gmail.com>
2023-11-22 21:26:20 +0300

    added script to update env file

---
+++ env_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Чтение значений из .env файла
+APP_KEY=$(awk -F '=' '/^APP_KEY=/ {print $2}' ./backend/.env)
+JWT_SECRET=$(awk -F '=' '/^JWT_SECRET=/ {print $2}' ./backend/.env)
+
+# Удаление .env файла
+rm -f ./backend/.env
+
+# Создание копии .env.example
+cp ./backend/.env.example ./backend/.env
+
+# Экранирование специальных символов для APP_KEY
+ESCAPED_APP_KEY=$(printf '%s\n' "$APP_KEY" | sed -e 's/[\/&]/\\&/g')
+ESCAPED_JWT_SECRET=$(printf '%s\n' "$JWT_SECRET" | sed -e 's/[\/&]/\\&/g')
+
+# Замена значений в новом .env файле
+sed -i '' "s|APP_KEY=.*|APP_KEY=$ESCAPED_APP_KEY|" ./backend/.env
+sed -i '' "s|JWT_SECRET=.*|JWT_SECRET=$ESCAPED_JWT_SECRET|" ./backend/.env
+
+# Удаляем composer lock чтобы актуализировать его при сборке 
+rm -f ./backend/composer.lock
+
+# Удаляем package lock чтобы актуализировать его при сборке 
+rm -f ./frontend/package-lock.json
