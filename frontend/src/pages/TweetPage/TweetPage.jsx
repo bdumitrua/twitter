9e6ed622ad82b9268e735070ea18ff9f09a6a498
carasique22 <timofeidudin@mail.ru>
2023-11-22 21:16:18 +0300

    tweet layout as in figma

--- frontend/src/pages/TweetPage/TweetPage.jsx
+++ frontend/src/pages/TweetPage/TweetPage.jsx
@@ -13,6 +13,7 @@ import retweet from "../../assets/images/Tweet/retweet.svg";
 import retweetWithComment from "../../assets/images/Tweet/retweetWithComment.svg";
 import unpaintedLike from "../../assets/images/Tweet/unpaintedLike.svg";
 import pictureExample from "../../assets/images/Tweet/pictureExample.jpg";
+import arrow from "../../assets/images/Tweet/arrow.svg";
 
 const RetweetModal = ({ onClose, showModal }) => (
 	<div className={`${styles["modal"]} ${showModal && styles["modal__open"]}`}>
@@ -58,88 +59,75 @@ const TweetPage = () => {
 	return (
 		<>
 			<Header />
-			<div className={styles["wrapper"]}>
-				<div className={styles["tweet"]}>
-					<div className={styles["tweet__wrapper"]}>
-						<div className={styles["tweet__content"]}>
-							<TweetAdditional />
-							<div className={styles["tweet__user-info"]}>
-								<UserAvatar />
-								<div className={styles["tweet__names"]}>
-									<span className={styles["tweet__username"]}>
-										Martha Craig
-									</span>
-									<span className={styles["tweet__nickname"]}>
-										@craig_love
-									</span>
-								</div>
-							</div>
-							<div className={styles["tweet__tweet-body"]}>
-								<div className={styles["tweet__text"]}>
-									{parseHashtags(
-										" ~~ hiring for a UX Lead in Sydney - who should I talk to? #TellMeAboutYou"
-									)}
-								</div>
-								<div
-									className={styles["tweet__picture-wrapper"]}
-								>
-									<img
-										className={styles["tweet__picture"]}
-										src={pictureExample}
-										alt=""
-									/>
-									<img
-										className={styles["tweet__picture"]}
-										src={pictureExample}
-										alt=""
-									/>
-									<img
-										className={styles["tweet__picture"]}
-										src={pictureExample}
-										alt=""
-									/>
-								</div>
-							</div>
-							<div className={styles["tweet__time-date"]}>
-								<div className={styles["tweet__time"]}>
-									09:28
-								</div>
-								<div className={styles["tweet__date"]}>
-									· 2/21/20
-								</div>
-							</div>
-							<div className={styles["tweet__counters"]}>
-								<div className={styles["tweet__retweets"]}>
-									<div className={styles["tweet__counter"]}>
-										6
-									</div>
-									<span>Retweets</span>
-								</div>
-								<div className={styles["tweet__likes"]}>
-									<div className={styles["tweet__counter"]}>
-										15
-									</div>
-									<span>Likes</span>
-								</div>
-							</div>
-							<div className={styles["tweet__buttons"]}>
-								<div className={styles["tweet__button"]}>
-									<img src={comment} alt="" />
-								</div>
-								<div
-									className={styles["tweet__button"]}
-									onClick={() => setShowModal(true)}
-								>
-									<img src={retweet} alt="" />
-								</div>
-								<div className={styles["tweet__button"]}>
-									<img src={unpaintedLike} alt="" />
-								</div>
-								<div className={styles["tweet__button"]}>
-									<img src={makeRepost} alt="" />
-								</div>
-							</div>
+			<div className={styles["tweet"]}>
+				<TweetAdditional />
+				<div className={styles["tweet__upper"]}>
+					<div className={styles["tweet__user"]}>
+						<UserAvatar />
+						<div className={styles["tweet__names"]}>
+							<span className={styles["tweet__username"]}>
+								Martha Craig
+							</span>
+							<span className={styles["tweet__nickname"]}>
+								@craig_love
+							</span>
 						</div>
+					</div>
+					<button className={styles["tweet__arrow"]}>
+						<img src={arrow} alt="" />
+					</button>
+				</div>
+				<div className={styles["tweet__text"]}>
+					{parseHashtags(
+						" ~~ hiring for a UX Lead in Sydney - who should I talk to? #TellMeAboutYou"
+					)}
+				</div>
+				<div className={styles["tweet__picture-wrapper"]}>
+					<img
+						className={styles["tweet__picture"]}
+						src={pictureExample}
+						alt=""
+					/>
+					<img
+						className={styles["tweet__picture"]}
+						src={pictureExample}
+						alt=""
+					/>
+					<img
+						className={styles["tweet__picture"]}
+						src={pictureExample}
+						alt=""
+					/>
+				</div>
+				<div className={styles["tweet__time-date"]}>
+					<div className={styles["tweet__time"]}>09:28</div>
+					<div className={styles["tweet__date"]}>· 2/21/20</div>
+				</div>
+				<div className={styles["tweet__counters"]}>
+					<div className={styles["tweet__retweets"]}>
+						<div className={styles["tweet__counter"]}>6</div>
+						<span>Retweets</span>
+					</div>
+					<div className={styles["tweet__likes"]}>
+						<div className={styles["tweet__counter"]}>15</div>
+						<span>Likes</span>
+					</div>
+				</div>
+				<div className={styles["tweet__actions"]}>
+					<div className={styles["tweet__action"]}>
+						<img src={comment} alt="" />
+					</div>
+					<div
+						className={styles["tweet__action"]}
+						onClick={() => setShowModal(true)}
+					>
+						<img src={retweet} alt="" />
+					</div>
+					<div className={styles["tweet__action"]}>
+						<img src={unpaintedLike} alt="" />
+					</div>
+					<div className={styles["tweet__action"]}>
+						<img src={makeRepost} alt="" />
 					</div>
 				</div>
 			</div>
