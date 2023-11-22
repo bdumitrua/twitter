e66661ade0638ff7dded60178d95c2b2c5ecfaca
carasique22 <timofeidudin@mail.ru>
2023-11-22 22:56:39 +0300

    link on user avatar

--- frontend/src/components/UserAvatar/UserAvatar.jsx
+++ frontend/src/components/UserAvatar/UserAvatar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import userPhoto from "../../assets/images/Tweet/userPhoto.svg";
 import styles from "../../assets/styles/components/UserAvatar/UserAvatar.module.scss";
 
 const UserAvatar = () => {
 	return (
-		<img className={styles["user-avatar"]} src={userPhoto} alt="" />
+		<Link to="/profile" className={styles["user-avatar__link"]}>
+			<img className={styles["user-avatar"]} src={userPhoto} alt="" />
+		</Link>
 	);
 };
 
