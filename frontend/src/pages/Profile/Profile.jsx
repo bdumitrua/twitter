18e641caa6f6b3e36a959b81a62b71ff1c88e26c
carasique22 <timofeidudin@mail.ru>
2023-11-25 01:57:34 +0300

    clickable tabs buttons

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -9,64 +9,130 @@ import linkIcon from "../../assets/images/Pages/Profile/linkIcon.svg";
 import userPhoto from "../../assets/images/Tweet/pictureExample.jpg";
 
 const Profile = () => {
+	const [activeTab, setActiveTab] = React.useState("tweets");
+
 	return (
-		<div className={styles["profile"]}>
-			<img className={styles["profile__banner"]} src={banner} alt="" />
-			<div className={styles["profile__bar"]}>
-				<div className={styles["profile__upper"]}>
-					<div className={styles["profile__avatar"]}>
-						<UserAvatar userPhoto={userPhoto} link="/profile" />
+		<>
+			<div className={styles["account"]}>
+				<div className={styles["profile"]}>
+					<img
+						className={styles["profile__banner"]}
+						src={banner}
+						alt=""
+					/>
+					<div className={styles["profile__bar"]}>
+						<div className={styles["profile__upper"]}>
+							<div className={styles["profile__avatar"]}>
+								<UserAvatar
+									userPhoto={userPhoto}
+									link="/profile"
+								/>
+							</div>
+							<button className={styles["profile__edit-button"]}>
+								Edit profile
+							</button>
+						</div>
+						<div className={styles["profile__user-data"]}>
+							<div className={styles["profile__username"]}>
+								Pixsellz
+							</div>
+							<div className={styles["profile__nickname"]}>
+								@pixsellz
+							</div>
+						</div>
+						<div className={styles["profile__biography"]}>
+							Digital Goodies Team - Web & Mobile UI/UX
+							development; Graphics; Illustrations
+						</div>
+						<div className={styles["profile__additional-data"]}>
+							<div className={styles["profile__link-block"]}>
+								<img src={linkIcon} alt="" />
+								<a
+									className={styles["profile__link"]}
+									href="https://bebra.io"
+								>
+									bebra.io
+								</a>
+							</div>
+							<div className={styles["profile__date-block"]}>
+								<img src={calendarIcon} alt="" />
+								<span className={styles["profile__date"]}>
+									Joined September 2007
+								</span>
+							</div>
+						</div>
+						<div className={styles["profile__counters"]}>
+							<div className={styles["profile__counter"]}>
+								<span
+									className={
+										styles["profile__counter-number"]
+									}
+								>
+									216
+								</span>
+								<span
+									className={styles["profile__counter-text"]}
+								>
+									Following
+								</span>
+							</div>
+							<div className={styles["profile__counter"]}>
+								<span
+									className={
+										styles["profile__counter-number"]
+									}
+								>
+									117
+								</span>
+								<span
+									className={styles["profile__counter-text"]}
+								>
+									Followers
+								</span>
+							</div>
+						</div>
 					</div>
-					<button className={styles["profile__edit-button"]}>
-						Edit profile
-					</button>
 				</div>
-				<div className={styles["profile__user-data"]}>
-					<div className={styles["profile__username"]}>Pixsellz</div>
-					<div className={styles["profile__nickname"]}>@pixsellz</div>
-				</div>
-				<div className={styles["profile__biography"]}>
-					Digital Goodies Team - Web & Mobile UI/UX development;
-					Graphics; Illustrations
-				</div>
-				<div className={styles["profile__additional-data"]}>
-					<div className={styles["profile__link-block"]}>
-						<img src={linkIcon} alt="" />
-						<a
-							className={styles["profile__link"]}
-							href="https://bebra.io"
+				<div className="tabs">
+					<div className={styles["tabs__row"]}>
+						<button
+							className={`${styles["tabs__tab"]} ${
+								activeTab === "tweets" ? styles["active"] : ""
+							}`}
+							onClick={() => setActiveTab("tweets")}
 						>
-							bebra.io
-						</a>
-					</div>
-					<div className={styles["profile__date-block"]}>
-						<img src={calendarIcon} alt="" />
-						<span className={styles["profile__date"]}>
-							Joined September 2007
-						</span>
-					</div>
-				</div>
-				<div className={styles["profile__counters"]}>
-					<div className={styles["profile__counter"]}>
-						<span className={styles["profile__counter-number"]}>
-							216
-						</span>
-						<span className={styles["profile__counter-text"]}>
-							Following
-						</span>
-					</div>
-					<div className={styles["profile__counter"]}>
-						<span className={styles["profile__counter-number"]}>
-							117
-						</span>
-						<span className={styles["profile__counter-text"]}>
-							Followers
-						</span>
+							Tweets
+						</button>
+						<button
+							className={`${styles["tabs__tab"]} ${
+								activeTab === "tweets-and-replies"
+									? styles["active"]
+									: ""
+							}`}
+							onClick={() => setActiveTab("tweets-and-replies")}
+						>
+							Tweets & replies
+						</button>
+						<button
+							className={`${styles["tabs__tab"]} ${
+								activeTab === "media" ? styles["active"] : ""
+							}`}
+							onClick={() => setActiveTab("media")}
+						>
+							Media
+						</button>
+						<button
+							className={`${styles["tabs__tab"]} ${
+								activeTab === "likes" ? styles["active"] : ""
+							}`}
+							onClick={() => setActiveTab("likes")}
+						>
+							Likes
+						</button>
 					</div>
 				</div>
 			</div>
-			<div className={styles["profile__tabs"]}></div>
-		</div>
+		</>
 	);
 };
 
