aa97331acf1e39290571043417389b45b54314e4
carasique22 <timofeidudin@mail.ru>
2023-11-28 00:31:56 +0300

    Typing text and changing textarea height depending on line count

--- frontend/src/pages/CreateTweet/CreateTweet.jsx
+++ frontend/src/pages/CreateTweet/CreateTweet.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+
+import styles from "../../assets/styles/pages/CreateTweet/CreateTweet.module.scss";
+import UserAvatar from "../../components/UserAvatar/UserAvatar";
+import userPhoto from "../../assets/images/Tweet/pictureExample.jpg";
 
 const CreateTweet = () => {
-  return (
-    <div>CreateTweet</div>
-  )
-}
+	const textareaRef = React.useRef(null);
 
-export default CreateTweet
+	const handleChange = () => {
+		const textarea = textareaRef.current;
+		textarea.style.height = "auto";
+		textarea.style.height = `${textarea.scrollHeight}px`;
+	};
+
+	return (
+		<div className={styles["create"]}>
+			<div className={styles["create__body"]}>
+				<div className={styles["create__input-container"]}>
+					<div className={styles["create__avatar"]}>
+						<UserAvatar userPhoto={userPhoto} link="/profile" />
+					</div>
+					<textarea
+						ref={textareaRef}
+						onChange={handleChange}
+						className={styles["create__input"]}
+						type="text"
+						placeholder="What's happening?"
+					></textarea>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CreateTweet;
