f4fcceb3f8c79ebbb23cad19c6320a381b5bbed2
carasique22 <timofeidudin@mail.ru>
2023-11-29 01:00:59 +0300

    Add line under avatar when typing some text

--- frontend/src/pages/CreateTweet/CreateTweet.jsx
+++ frontend/src/pages/CreateTweet/CreateTweet.jsx
@@ -5,20 +5,33 @@ import UserAvatar from "../../components/UserAvatar/UserAvatar";
 import userPhoto from "../../assets/images/Tweet/pictureExample.jpg";
 
 const CreateTweet = () => {
+	const [showLine, setShowLine] = React.useState(false);
 	const textareaRef = React.useRef(null);
+	const minHeight = 61;
 
 	const handleChange = () => {
 		const textarea = textareaRef.current;
 		textarea.style.height = "auto";
 		textarea.style.height = `${textarea.scrollHeight}px`;
+
+		if (textarea.scrollHeight < minHeight) {
+			setShowLine(false);
+		} else {
+			setShowLine(true);
+		}
 	};
 
 	return (
 		<div className={styles["create"]}>
 			<div className={styles["create__body"]}>
 				<div className={styles["create__input-container"]}>
-					<div className={styles["create__avatar"]}>
-						<UserAvatar userPhoto={userPhoto} link="/profile" />
+					<div className={styles["create__avatar-group"]}>
+						<div className={styles["create__avatar"]}>
+							<UserAvatar userPhoto={userPhoto} link="/profile" />
+						</div>
+						{showLine && (
+							<div className={styles["create__avatar-line"]} />
+						)}
 					</div>
 					<textarea
 						ref={textareaRef}
