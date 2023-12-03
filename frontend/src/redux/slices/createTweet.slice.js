f0ac543e6b43156393c4eb194c0b66131fd4f88a
carasique22 <timofeidudin@mail.ru>
2023-12-03 16:18:35 +0300

    quickfix

--- frontend/src/redux/slices/createTweet.slice.js
+++ frontend/src/redux/slices/createTweet.slice.js
@@ -4,6 +4,7 @@ const createTweetSlice = createSlice({
 	name: "createTweet",
 	initialState: {
 		tweetBodies: [],
+		nextId: 0,
 		currentId: 0,
 	},
 	reducers: {
@@ -14,8 +15,10 @@ const createTweetSlice = createSlice({
 				charCount: 0,
 			};
 			state.tweetBodies.push(newBody);
+			state.nextId++;
 		},
-		removeTweetBody: (state, action) => {},
+		removeTweetBody: (state, action) => {
+		},
 		updateTweetBodyLength: (state, action) => {
 			const { id, charCount } = action.payload;
 			const tweetBody = state.tweetBodies.find((body) => body.id === id);
