24c19e0bfcc01f4651d17823bf5f61b5c66cb022
carasique22 <timofeidudin@mail.ru>
2023-12-03 16:23:51 +0300

    quickfix

--- frontend/src/pages/CreateTweet/CreateTweet.jsx
+++ frontend/src/pages/CreateTweet/CreateTweet.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import {
 	addTweetBody,
 	selectTweetBodies,
-	selectNextId,
 } from "../../redux/slices/createTweet.slice";
 
 import styles from "../../assets/styles/pages/CreateTweet/CreateTweet.module.scss";
