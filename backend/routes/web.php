8a103f4841015f1bd9fdacb3d1fcf3e7a40dd81c
bdumitrua <bdumitrua@gmail.com>
2023-12-04 19:01:02 +0300

    replaced Redis config for metrics in AppServiceProvider

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,23 +15,7 @@ use Prometheus\RenderTextFormat;
 |
 */
 
-Route::get('/', function () {
-    // 
-});
-
 Route::get('/metrics', function () {
-    \Prometheus\Storage\Redis::setDefaultOptions(
-        [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'port' => env('REDIS_PORT', '6379'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'timeout' => 0.1, // in seconds
-            'read_timeout' => '10', // in seconds
-            'persistent_connections' => false
-        ]
-    );
-
     $registry = CollectorRegistry::getDefault();
     $renderer = new RenderTextFormat();
     $result = $renderer->render($registry->getMetricFamilySamples());
@@ -40,20 +24,6 @@ Route::get('/metrics', function () {
 });
 
 Route::get('/test-metric', function () {
-    \Prometheus\Storage\Redis::setDefaultOptions(
-        [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'port' => env('REDIS_PORT', '6379'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'timeout' => 0.1, // in seconds
-            'read_timeout' => '10', // in seconds
-            'persistent_connections' => false
-        ]
-    );
-
     $counter = CollectorRegistry::getDefault()->getOrRegisterCounter('twitter', 'test_metric_counter', 'Counter for test metric');
     $counter->inc();
-
-    return "Test metric incremented";
 });
