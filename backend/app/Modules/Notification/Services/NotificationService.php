aa7f1f0afbf1c606de0dd020a9503ffa7b34de57
bdumitrua <bdumitrua@gmail.com>
2023-12-04 22:40:51 +0300

    logged Notification module (without consumers)

--- backend/app/Modules/Notification/Services/NotificationService.php
+++ backend/app/Modules/Notification/Services/NotificationService.php
@@ -10,17 +10,21 @@ use App\Modules\Notification\Models\Notification;
 use App\Modules\Notification\Repositories\NotificationRepository;
 use App\Modules\Notification\Requests\UpdateNotificationStatusRequest;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\Facades\Auth;
 
 class NotificationService
 {
-    private $notificationRepository;
-    private $authorizedUserId;
+    private NotificationRepository $notificationRepository;
+    protected LogManager $logger;
+    private int $authorizedUserId;
 
     public function __construct(
-        NotificationRepository $notificationRepository
+        NotificationRepository $notificationRepository,
+        LogManager $logger,
     ) {
         $this->notificationRepository = $notificationRepository;
+        $this->logger = $logger;
         $this->authorizedUserId = Auth::id();
     }
 
@@ -31,11 +35,19 @@ class NotificationService
 
     public function create(NotificationDTO $notificationDTO): void
     {
+        $this->logger->info('Creating notification from request DTO', $notificationDTO->toArray());
         $this->notificationRepository->create($notificationDTO);
     }
 
     public function update(Notification $notification, UpdateNotificationStatusRequest $updateNotificationStatusRequest): void
     {
+        $this->logger->info(
+            'Updating notification from update request',
+            [
+                'notification_uuid' => $notification->uuid,
+                'Request' => $updateNotificationStatusRequest->toArray()
+            ]
+        );
         $this->notificationRepository->update($notification, $updateNotificationStatusRequest->status);
     }
 }
