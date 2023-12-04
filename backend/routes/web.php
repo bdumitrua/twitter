ecef66113d17219b78753c7d8cc8bb201d14ff36
bdumitrua <bdumitrua@gmail.com>
2023-12-04 18:58:23 +0300

    added test metric

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Jobs\NewJob;
 use Illuminate\Support\Facades\Route;
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,43 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // 
+});
+
+Route::get('/metrics', function () {
+    \Prometheus\Storage\Redis::setDefaultOptions(
+        [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'port' => env('REDIS_PORT', '6379'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'timeout' => 0.1, // in seconds
+            'read_timeout' => '10', // in seconds
+            'persistent_connections' => false
+        ]
+    );
+
+    $registry = CollectorRegistry::getDefault();
+    $renderer = new RenderTextFormat();
+    $result = $renderer->render($registry->getMetricFamilySamples());
+
+    return response($result)->header('Content-Type', RenderTextFormat::MIME_TYPE);
+});
+
+Route::get('/test-metric', function () {
+    \Prometheus\Storage\Redis::setDefaultOptions(
+        [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'port' => env('REDIS_PORT', '6379'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'timeout' => 0.1, // in seconds
+            'read_timeout' => '10', // in seconds
+            'persistent_connections' => false
+        ]
+    );
+
+    $counter = CollectorRegistry::getDefault()->getOrRegisterCounter('twitter', 'test_metric_counter', 'Counter for test metric');
+    $counter->inc();
+
+    return "Test metric incremented";
 });
