983836e42c19776eeb6e5a6a111099214cc31f32
bdumitrua <bdumitrua@gmail.com>
2023-12-06 21:45:02 +0300

    added names to Auth module routes

--- backend/app/Modules/Auth/Routes/authRoutes.php
+++ backend/app/Modules/Auth/Routes/authRoutes.php
@@ -8,29 +8,28 @@ use App\Modules\Auth\Controllers\AuthController;
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::prefix('registration')->group(function () {
         // Начать регистрацию
-        Route::post('start', 'registrationStart');
+        Route::post('start', 'registrationStart')->name('start_registration');
         // Подтвердить регистрацию кодом
-        Route::post('confirm/{authRegistration}', 'registrationConfirm');
+        Route::post('confirm/{authRegistration}', 'registrationConfirm')->name('confirm_registration_code');
         // Зарегистрироваться
-        Route::post('end/{authRegistration}', 'registrationEnd');
+        Route::post('end/{authRegistration}', 'registrationEnd')->name('end_registration');
     });
 
     Route::prefix('reset')->group(function () {
         // Проверить существование аккаунта по почте
-        Route::get('check', 'resetCheck');
+        Route::get('check', 'resetCheck')->name('check_email_for_password_reset');
         // Подтвердить сборс пароля кодом
-        Route::post('confirm/{authReset}', 'resetConfirm');
+        Route::post('confirm/{authReset}', 'resetConfirm')->name('confirm_password_reset_code');
         // Изменить пароль аккаунта
-        Route::post('end/{authReset}', 'resetEnd');
+        Route::post('end/{authReset}', 'resetEnd')->name('end_password_reseting');
     });
 
     // Залогиниться
-    Route::post('login', 'login')->name('auth.login');
+    Route::post('login', 'login')->name('auth_login');
     // Получить новый токен (по уже истёкшему)
-    Route::get('refresh', 'refresh');
-
+    Route::get('refresh', 'refresh')->name('auth_refresh_token');
     Route::middleware(['auth:api'])->group(function () {
         // Выйти
-        Route::post('logout', 'logout');
+        Route::post('logout', 'logout')->name('auth_logout');
     });
 });
