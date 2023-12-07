311bc6ad2cc49485b8ad90a0eca535e9392b566f
Skobelev Matvey <mask0343@gmail.com>
2023-12-07 14:16:21 +0300

    store ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,8 +1,8 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/auth.slice";
+import createTweetSlice from "./slices/createTweet.slice";
 import registerSlice from "./slices/register.slice";
 import userSlice from "./slices/user.slice";
-import createTweetSlice from "./slices/createTweet.slice";
 
 const rootReducer = combineReducers({
 	auth: authSlice,
@@ -15,4 +15,6 @@ const store = configureStore({
 	reducer: rootReducer,
 });
 
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
