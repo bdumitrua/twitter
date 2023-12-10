692293fdf0173ccffc1c21c07d0f875b62e3caef
bdumitrua <bdumitrua@gmail.com>
2023-12-10 17:28:05 +0300

    changed user groups routes

--- backend/app/Modules/User/Routes/userGroupRoutes.php
+++ backend/app/Modules/User/Routes/userGroupRoutes.php
@@ -8,15 +8,15 @@ Route::prefix('users/groups')->middleware(['auth:api'])->controller(UserGroupCon
     // Получить мои группы 
     Route::get('/', 'index')->name('get_user_groups');
     // Создать группу
-    Route::post('create', 'create')->name('create_user_group');
+    Route::post('/', 'create')->name('create_user_group');
 
     Route::middleware(['checkRights:userGroup'])->group(function () {
         // Получить мои группы 
-        Route::get('show/{userGroup}', 'show')->name('show_user_group');
+        Route::get('{userGroup}', 'show')->name('show_user_group');
         // Изменить данные группы
-        Route::patch('update/{userGroup}', 'update')->name('update_user_group');
+        Route::patch('{userGroup}', 'update')->name('update_user_group');
         // Удалить группу
-        Route::delete('destroy/{userGroup}', 'destroy')->name('destroy_user_group');
+        Route::delete('{userGroup}', 'destroy')->name('destroy_user_group');
 
         // Добавить пользователя в группу
         Route::post('add/{userGroup}/{user}', 'add')->name('add_user_to_user_group');
