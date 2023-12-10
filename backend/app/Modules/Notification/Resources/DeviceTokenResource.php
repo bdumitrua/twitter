b4b97424b33dbf28c9dab09e2a9d08d53c6a9f8f
bdumitrua <bdumitrua@gmail.com>
2023-12-10 20:29:12 +0300

    added actions to DeviceTokenResource

--- backend/app/Modules/Notification/Resources/DeviceTokenResource.php
+++ backend/app/Modules/Notification/Resources/DeviceTokenResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Notification\Resources;
 
+use App\Http\Resources\ActionsResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -9,10 +10,24 @@ class DeviceTokenResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $actions = ActionsResource::collection([
+            [
+                "UpdateDeviceToken",
+                "update_device_token",
+                ["deviceToken" => $this->id]
+            ],
+            [
+                "DeleteDeviceToken",
+                "delete_device_token",
+                ["deviceToken" => $this->id]
+            ],
+        ]);
+
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
             'token' => $this->token,
+            'actions' => $actions,
         ];
     }
 }
