9c7044331c0c894fed4a205b590690b65298cc94
Skobelev Matvey <mask0343@gmail.com>
2023-12-10 18:10:42 +0300

    use scrollToTop

--- frontend/src/pages/DefaultLayout.tsx
+++ frontend/src/pages/DefaultLayout.tsx
@@ -1,9 +1,12 @@
+import useScrollToTop from "@/utils/hooks/useScrollToTop";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
 const DefaultLayout: React.FC = () => {
 	const haveUnwatched: boolean = true;
+
+	useScrollToTop();
 
 	return (
 		<div>
