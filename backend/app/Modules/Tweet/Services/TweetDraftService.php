1568fd54076aeaee73e449d7fa83d22ae8c59d21
bdumitrua <bdumitrua@gmail.com>
2023-12-10 03:01:47 +0300

    added logging and soft deletes

--- backend/app/Modules/Tweet/Services/TweetDraftService.php
+++ backend/app/Modules/Tweet/Services/TweetDraftService.php
@@ -7,17 +7,21 @@ use App\Modules\Tweet\Requests\CreateTweetDraftRequest;
 use App\Modules\Tweet\Requests\RemoveTweetDraftsRequest;
 use App\Modules\Tweet\Resources\TweetDraftResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\Facades\Auth;
 
 class TweetDraftService
 {
     private TweetDraftRepository $tweetDraftRepository;
+    protected LogManager $logger;
     protected ?int $authorizedUserId;
 
     public function __construct(
-        TweetDraftRepository $tweetDraftRepository
+        TweetDraftRepository $tweetDraftRepository,
+        LogManager $logger,
     ) {
         $this->tweetDraftRepository = $tweetDraftRepository;
+        $this->logger = $logger;
         $this->authorizedUserId = Auth::id();
     }
 
@@ -30,6 +34,7 @@ class TweetDraftService
 
     public function create(CreateTweetDraftRequest $request): void
     {
+        $this->logger->info('Creating tweet draft from create request', $request->toArray());
         $this->tweetDraftRepository->create($request->text, $this->authorizedUserId);
     }
 
--- backend/database/migrations/2023_12_09_225243_create_tweet_drafts_table.php
+++ backend/database/migrations/2023_12_09_225243_create_tweet_drafts_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->softDeletes();
         });
     }
 
