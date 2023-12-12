1d9638cb2016b6648359a1cc8bf3a826a7a2c481
Skobelev Matvey <mask0343@gmail.com>
2023-12-12 16:16:09 +0300

    rm cl

--- frontend/src/utils/functions/fetchData.ts
+++ frontend/src/utils/functions/fetchData.ts
@@ -2,6 +2,6 @@ import axiosInstance from "../axios/instance";
 
 export const fetchData = async (path: string) => {
 	const response = await axiosInstance.get(path);
-	console.log(response.data);
+
 	return response.data;
 };
