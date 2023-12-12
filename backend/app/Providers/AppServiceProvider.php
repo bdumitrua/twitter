437a31e037db7a04c1ade3e623741c9e1571f975
bdumitrua <bdumitrua@gmail.com>
2023-12-12 16:20:59 +0300

    disabled grafana/redis_exporter/kibana for optimization; switched logs to file instead of elasticsearch

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -29,7 +29,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
         DB::listen(function ($query) {
             /** @var PrometheusService */
             $prometheusService = app(PrometheusService::class);
--- backend/config/logging.php
+++ backend/config/logging.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'elasticsearch'),
+    // 'default' => env('LOG_CHANNEL', 'elasticsearch'),
+    'default' => 'file',
 
     /*
     |--------------------------------------------------------------------------
@@ -47,25 +48,33 @@ return [
     */
 
     'channels' => [
-        'elasticsearch' => [
-            'driver' => 'monolog',
-            'handler' => Monolog\Handler\ElasticsearchHandler::class,
-            'handler_with' => [
-                'client' => (new Elastic\Elasticsearch\ClientBuilder())->setHosts(['elasticsearch:9200'])->build(),
-                'options' => [
-                    'index' => 'laravel-logs',
-                    'type'  => '_doc',
-                ],
-            ],
-            'formatter' => Monolog\Formatter\ElasticsearchFormatter::class,
-            'formatter_with' => [
-                'index' => 'laravel-logs',
-                'type'  => '_doc',
-            ],
-        ],
+        // 'elasticsearch' => [
+        //     'driver' => 'monolog',
+        //     'handler' => Monolog\Handler\ElasticsearchHandler::class,
+        //     'handler_with' => [
+        //         'client' => (new Elastic\Elasticsearch\ClientBuilder())->setHosts(['elasticsearch:9200'])->build(),
+        //         'options' => [
+        //             'index' => 'laravel-logs',
+        //             'type'  => '_doc',
+        //         ],
+        //     ],
+        //     'formatter' => Monolog\Formatter\ElasticsearchFormatter::class,
+        //     'formatter_with' => [
+        //         'index' => 'laravel-logs',
+        //         'type'  => '_doc',
+        //     ],
+        // ],
 
         'emergency' => [
+            'driver' => 'single', // Указываем драйвер
             'path' => storage_path('logs/emergency.log'),
+            'level' => 'error', // Уровень логирования, можно изменить по потребности
+        ],
+
+        'file' => [
+            'driver' => 'single', // Также указываем драйвер для канала 'file'
+            'path' => storage_path('logs/logs.log'),
+            'level' => 'debug', // Уровень логирования
         ],
     ],
 
