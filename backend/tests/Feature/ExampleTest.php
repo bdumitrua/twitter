572fde16b47d84aab744bb921eb3d3bf3aac67ea
bdumitrua <bdumitrua@gmail.com>
2023-12-20 19:58:56 +0300

    test work

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -4,20 +4,24 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Modules\Tweet\Models\Tweet;
 use App\Modules\User\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
         $user = User::factory()->create();
-        var_dump($user);
-        $response = $this->get('/');
+        $tweet = Tweet::factory()->create(['user_id' => $user->id]);
+        var_dump($tweet);
+        $response = $this->get('/api/tweets/show/1');
 
-        $response->assertStatus(404);
+        $response->assertStatus(200);
     }
 }
