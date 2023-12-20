21ee79354e27242754f351f950bc52dda303d3ef
bdumitrua <bdumitrua@gmail.com>
2023-12-20 16:06:28 +0300

    added indexes to table

--- backend/database/migrations/2023_12_20_124302_create_notifications_subscribers_table.php
+++ backend/database/migrations/2023_12_20_124302_create_notifications_subscribers_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('notifications_subscribtions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('subscriber_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->unsignedBigInteger('subscriber_id')->index();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
