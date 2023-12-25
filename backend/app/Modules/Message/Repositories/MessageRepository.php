ec1cbf23fb1d3e3e50416bd0df5d12a39c47d281
bdumitrua <bdumitrua@gmail.com>
2023-12-25 21:26:50 +0300

    service v1

--- backend/app/Modules/Message/Repositories/MessageRepository.php
+++ backend/app/Modules/Message/Repositories/MessageRepository.php
@@ -2,8 +2,13 @@
 
 namespace App\Modules\Message\Repositories;
 
+use App\Helpers\ResponseHelper;
+use App\Modules\Message\DTO\MessageDTO;
 use App\Modules\Message\Models\Chat;
 use App\Modules\Message\Models\Message;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Response;
 
 class MessageRepository
 {
@@ -13,5 +18,68 @@ class MessageRepository
         Chat $chat
     ) {
         $this->chat = $chat;
+    }
+
+    protected function queryUserChats(int $userId): Builder
+    {
+        return $this->chat
+            ->whereIn('first_user_id', $userId)
+            ->orWhereIn('second_user_id', $userId)
+            ->latest('updated_at');
+    }
+
+    protected function queryChatByParticipants(array $participants): Builder
+    {
+        return $this->chat
+            ->whereIn('first_user_id', $participants)
+            ->whereIn('second_user_id', $participants);
+    }
+
+    public function findOrCreateChat(array $participants): Chat
+    {
+        $chat = $this->queryChatByParticipants($participants)->first();
+
+        if (empty($chat)) {
+            $chat = $this->create($participants);
+        }
+
+        return $chat;
+    }
+
+    public function getChatMessages(array $participants): array
+    {
+        return [];
+    }
+
+    public function getUserChats(int $userId): Collection
+    {
+        $chats = $this->queryUserChats($userId)->get();
+
+        return $chats;
+    }
+
+    public function create(array $participants): Chat
+    {
+        $data = [
+            'first_user_id' => $participants[0],
+            'second_user_id' => $participants[1],
+        ];
+
+        return $this->chat->create($data);
+    }
+
+    public function send(MessageDTO $messageDTO): void
+    {
+        // 
+    }
+
+    public function read(int $messageId): Response
+    {
+        return ResponseHelper::okResponse();
+    }
+
+    public function delete(int $messageId): Response
+    {
+        return ResponseHelper::okResponse();
     }
 }
--- backend/app/Modules/Message/Services/MessageService.php
+++ backend/app/Modules/Message/Services/MessageService.php
@@ -2,48 +2,86 @@
 
 namespace App\Modules\Message\Services;
 
+use App\Exceptions\UnprocessableContentException;
+use App\Modules\Message\DTO\MessageDTO;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Modules\Message\Repositories\MessageRepository;
 use App\Modules\Message\Requests\MessageRequest;
 use App\Modules\User\Models\User;
+use App\Traits\CreateDTO;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\Facades\Auth;
 
 class MessageService
 {
+    use CreateDTO;
+
     protected MessageRepository $messageRepository;
+    protected LogManager $logger;
     protected ?int $authorizedUserId;
 
-
     public function __construct(
-        MessageRepository $messageRepository
+        MessageRepository $messageRepository,
+        LogManager $logger,
     ) {
         $this->messageRepository = $messageRepository;
+        $this->logger = $logger;
         $this->authorizedUserId = Auth::id();
-    }
-
-    public function index(User $user)
-    {
-        // 
-    }
-
-    public function send(User $user, MessageRequest $messageRequest)
-    {
-        //
-    }
-
-    public function read(int $messageId)
-    {
-        // 
-    }
-
-    public function delete(int $messageId)
-    {
-        // 
     }
 
     public function chats()
     {
-        // 
+        return $this->messageRepository->getUserChats($this->authorizedUserId);
+    }
+
+    public function index(User $user)
+    {
+        $participants = [
+            $user->id,
+            $this->authorizedUserId
+        ];
+
+        return $this->messageRepository->getChatMessages($participants);
+    }
+
+    public function send(User $user, MessageRequest $messageRequest)
+    {
+        $this->logger->info('Validating message data', $messageRequest->toArray());
+        $this->validateMessageRequest($messageRequest);
+
+        $participants = [
+            $user->id,
+            $this->authorizedUserId
+        ];
+        $this->messageRepository->findOrCreateChat($participants);
+
+        $messageDTO = $this->createDTO($messageRequest, MessageDTO::class);
+        $messageDTO->senderId = $this->authorizedUserId;
+        $messageDTO->receiverId = $user->id;
+
+        $this->logger->info('Sending message from messageDTO', $messageDTO->toArray());
+        $this->messageRepository->send($messageDTO);
+    }
+
+    public function read(int $messageId): Response
+    {
+        return $this->messageRepository->read($messageId);
+    }
+
+    public function delete(int $messageId): Response
+    {
+        return $this->messageRepository->delete($messageId);
+    }
+
+    protected function validateMessageRequest(MessageRequest $messageRequest): void
+    {
+        if (empty($messageRequest->text) && empty($messageRequest->linkedEntityId)) {
+            throw new UnprocessableContentException('Message can\'t be empty');
+        }
+
+        if (is_null($messageRequest->linkedEntityId) !== is_null($messageRequest->linkedEntityType)) {
+            throw new UnprocessableContentException('Оба поля linkedEntityId и linkedEntityType должны быть заполнены или пустыми');
+        }
     }
 }
