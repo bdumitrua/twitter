42714a613fc894eccf1aa3c4136a5f3413fd62a5
carasique22 <timofeidudin@mail.ru>
2023-12-29 01:29:07 +0300

    remove log

--- frontend/src/redux/slices/user.slice.ts
+++ frontend/src/redux/slices/user.slice.ts
@@ -4,7 +4,6 @@ import UserService from "../services/user.service";
 
 export const getMeAsync = createAsyncThunk("user/me", async () => {
 	const user = await UserService.getMe();
-	console.log(user);
 
 	return user;
 });
